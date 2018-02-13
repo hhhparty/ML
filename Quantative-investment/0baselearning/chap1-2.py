d5f07939e4290a49a665b4adacaa3dc5740281d7
leo <hhhparty@163.com>
2018-02-13 20:53:52 +0800

    Update chap1-2.py

--- Quantative-investment/0baselearning/chap1-2.py
+++ Quantative-investment/0baselearning/chap1-2.py
@@ -20,16 +20,16 @@ def sta001(k,nyear,xd):
     d2 = round(d2)
     return d2
 
-d40 = 1.4 * 40
-print("d40,40 * 1.4 = ",d40)
-d = sta001(0.05,40-1,1.4)
-print("01 Conservative investment mode：", d, round(d/d40))
-
-d2 = sta001(0.2,40-1,1.4)
-print("02 Radical investment mode：", d2, round(d2/d40))
-
-dk = round(d2/d)
-print("dk, the difference of two above mode: ",dk)
+def dr_xtyp(_dat):
+    i = 0
+    for xss in plt.style.available:
+        plt.figure()
+        plt.style.use(xss) # This is a important usage!
+        _dat.plot()
+        fss = "C:\\Users\\leo\\Desktop\\ml\\" + xss + ".png"
+        i += 1;
+        print(i,xss,",",fss)
+        plt.show()
 
 dx05 = [sta001(0.05,x,1.4) for x in range(0,40)]
 dx10 = [sta001(0.10,x,1.4) for x in range(0,40)] 
@@ -42,4 +42,4 @@ df['dx10'] = dx10
 df['dx15'] = dx15
 df['dx20'] = dx20
 print(df.tail())
-df.plot()
+dr_xtyp(df)
